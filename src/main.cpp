5c844f65e9d85aecf731ab034884960084c42b4a
Cedric Martens <cedricmartens98@gmail.com>
2021-07-07 21:55:23 -0500

    Add SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
 #include "util/safety.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+#include "CPU.h"
+
+
 
 int main() {
 
-    validate_argument<uint16_t>(0xFF, 0x3);
-    std::cout << "Hello, World!" << std::endl;
+    //CPU cpu;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+    }
+
+    bool running = true;
+    bool stopped = false;
+    SDL_Window* window = SDL_CreateWindow("EmuBoy",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       800, 800, 0);
+    while (running)
+    {
+        SDL_Event event;
+
+        while(SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+                case SDL_QUIT:
+                    running = false;
+                    SDL_Quit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if(!stopped)
+        {
+//            cpu.fetch_cycle();
+        }
+    }
+
     return 0;
 }
